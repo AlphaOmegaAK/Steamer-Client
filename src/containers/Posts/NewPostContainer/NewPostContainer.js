b63f51e1f4ec9ea1ca64d34f6460a3c067be0559
AlphaOmegaAK <alphaunionxt@gmail.com>
2020-08-24 18:20:38 -0400

    posts create done, still need to code out saving it to logged in user

--- src/containers/Posts/NewPostContainer/NewPostContainer.js
+++ src/containers/Posts/NewPostContainer/NewPostContainer.js
@@ -1,33 +1,54 @@
 import React from 'react';
-
+import PostModel from '../../../models/PostModel';
+import UserModel from '../../../models/UserModel';
 
 class NewPostContainer extends React.Component {
+  state = {
+    title: '',
+    topic: '',
+    body: '',
+    user: [],
+  }
 
+  handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ [event.target.name]: event.target.value })
+    // looking for name property on input tag
+  }
+
+  handleSubmit = (event) => {
+    event.prevenDefault();
+    PostModel.createPost(this.state)
+      .then((result) => {
+        console.log(result);
+      })
+    this.props.history.push('/posts')
+  }
 
   render() {
     return (
       <div>
         <h2>
           Send off a New Post
-           <form className="form-container" >
+           <form className="form-container" onSubmit={this.handleSubmit}>
             <div>
               <label htmlFor="">Title</label>
-              <input type="text" name="title" />
+              <input onInput={this.handleChange} type="text" name="title" />
             </div>
             <div>
               <label htmlFor="">Topic</label>
-              <input type="text" name="topic" />
+              <input onInput={this.handleChange} type="text" name="topic" />
             </div>
             <div>
               <label htmlFor="">Body</label>
-              <input type="text" name="body" />
+              <input onInput={this.handleChange} type="text" name="body" />
             </div>
-
+            <button type="submit">Post</button>
           </form>
         </h2>
       </div>
     )
-  }
-}
+  };
+};
 
 export default NewPostContainer;
--- src/models/PostModel.js
+++ src/models/PostModel.js
@@ -11,6 +11,23 @@ class PostModel {
     return fetch(`${url}/${postId}`)
       .then((res) => res.json())
   }
+  static createPost = (postObj) => {
+    return fetch(url, {
+      // configuration object anytime you want to do anything other than a GET request
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // Date itself we want to send
+      // if axios its datta
+      body: JSON.stringify
+      // stringify converst objects to strings
+    })
+      .then((res) => res.json())
+  }
 }
 
 export default PostModel;
+
+// For Delete you can leave off body/data-axios
+// update needs body/data-axios
