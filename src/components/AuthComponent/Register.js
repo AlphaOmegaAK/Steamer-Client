1237add92981b94fc1c8cb4ad822394ebaa4a602
AlphaOmegaAK <alphaunionxt@gmail.com>
2020-08-24 19:30:31 -0400

    updated form

--- src/components/AuthComponent/Register.js
+++ src/components/AuthComponent/Register.js
@@ -4,36 +4,43 @@ import React from 'react';
 class Register extends React.Component {
   state = {
     firstName: '',
+    lastName: '',
     email: '',
     password: '',
+    location: '',
   }
 
   render() {
     return (
       <form onSubmit="">
         <div className="form-group">
-          <label htmlFor="firstName">First Name</label>
-          <input type="text" />
+          <label htmlFor="firstName">First Name : </label>
+          <input type="text" onChange="" id="" name="" value="" />
         </div>
         <div className="form-group">
-          <label htmlFor="lastName">Last Name</label>
-          <input type="text" />
+          <label htmlFor="lastName">Last Name : </label>
+          <input type="text" onChange="" id="" name="" value="" />
         </div>
         <div className="form-group">
-          <label htmlFor="username">User Name</label>
-          <input type="text" />
+          <label htmlFor="username">User Name : </label>
+          <input type="text" onChange="" id="" name="" value="" />
         </div>
 
         <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="text" />
-          <label htmlFor="password">Password</label>
-          <input type="text" />
+          <label htmlFor="email">Email :</label>
+          <input type="email" onChange="" id="" name="" value="" />
         </div>
         <div className="form-group">
-          <label htmlFor="location">Location</label>
-          <input type="text" />
+          <label htmlFor="password">Password :</label>
+          <input type="password" onChange="" id="" name="" value="" />
+          <label htmlFor="password">Password :</label>
+          <input type="password" onChange="" id="" name="" value="" />
         </div>
+        <div className="form-group">
+          <label htmlFor="location">Location :</label>
+          <input type="text" onChange="" id="" name="" value="" />
+        </div>
+        <button onSubmit="">Complete Register</button>
       </form>
     )
   };
