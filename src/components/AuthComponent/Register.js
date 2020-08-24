80e2d62031851cf5612e8c06929004a2b7ba4874
AlphaOmegaAK <alphaunionxt@gmail.com>
2020-08-24 19:28:15 -0400

    Basic Register Form

--- src/components/AuthComponent/Register.js
+++ src/components/AuthComponent/Register.js
@@ -2,9 +2,39 @@ import React from 'react';
 
 
 class Register extends React.Component {
+  state = {
+    firstName: '',
+    email: '',
+    password: '',
+  }
+
   render() {
     return (
-      <h1>Register</h1>
+      <form onSubmit="">
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="username">User Name</label>
+          <input type="text" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="text" />
+          <label htmlFor="password">Password</label>
+          <input type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <input type="text" />
+        </div>
+      </form>
     )
   };
 };
