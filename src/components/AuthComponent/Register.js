5936bd8a159ad65a1e9a703a578f07275fe11b8b
AlphaOmegaAK <alphaunionxt@gmail.com>
2020-08-25 06:34:41 -0400

    regist form itself complete next is axios call to save

--- src/components/AuthComponent/Register.js
+++ src/components/AuthComponent/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class Register extends React.Component {
@@ -8,37 +9,61 @@ class Register extends React.Component {
     email: '',
     password: '',
     location: '',
+
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post()
+      .then((res) => {
+        console.log(res);
+        this.props.history.push('/login')
+      })
+      .catch((err) => {
+        console.log(err.response.status);
+        console.log(err.response.data);
+        console.log(err.response.data.message);
+
+      });
+
   }
 
   render() {
     return (
-      <form onSubmit=''>
+      <form onSubmit={this.handleSubmit}>
         <div className="form-group">
           <label htmlFor="firstName">First Name : </label>
-          <input type="text" onChange="" id="firstName" name="firstName" value={this.state.firstName} />
+          <input type="text" onChange={this.handleChange} id="firstName" name="firstName" value={this.state.firstName} />
         </div>
         <div className="form-group">
           <label htmlFor="lastName">Last Name : </label>
-          <input type="text" onChange="" id="lastName" name="lastName" value={this.state.lastName} />
+          <input type="text" onChange={this.handleChange} id="lastName" name="lastName" value={this.state.lastName} />
         </div>
         <div className="form-group">
           <label htmlFor="username">User Name : </label>
-          <input type="text" onChange="" id="username" name="username" value={this.state.username} />
+          <input type="text" onChange={this.handleChange} id="username" name="username" value={this.state.username} />
         </div>
 
         <div className="form-group">
           <label htmlFor="email">Email :</label>
-          <input type="email" onChange="" id="email" name="email" value={this.state.email} />
+          <input type="email" onChange={this.handleChange} id="email" name="email" value={this.state.email} />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password :</label>
-          <input type="password" onChange="" id="password" name="password" value={this.state.password} />
+          <input type="password" onChange={this.handleChange} id="password" name="password" value={this.state.password} />
           <label htmlFor="password">Password :</label>
-          <input type="password" onChange="" id="password" name="password" value={this.state.password} />
+          <input type="password" onChange={this.handleChange} id="password" name="password" value={this.state.password} />
         </div>
         <div className="form-group">
           <label htmlFor="location">Location :</label>
-          <input type="text" onChange="" id="location" name="location" value="location" />
+          <input type="text" onChange={this.handleChange} id="location" name="location" value={this.state.location} />
         </div>
         <button onSubmit="">Complete Register</button>
       </form>
