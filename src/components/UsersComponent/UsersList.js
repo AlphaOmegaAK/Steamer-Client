0da33a78c95cec4fe004ec1d89ec0b880fc24d6a
AlphaOmegaAK <alphaunionxt@gmail.com>
2020-08-26 12:32:44 -0400

    User List created with User Card

--- src/components/UsersComponent/UsersList.js
+++ src/components/UsersComponent/UsersList.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import UserCard from './UserCard';
+
+
+function UserList(props) {
+  const userList = props.users.map((userObj) => {
+    return <UserCard
+      key={userObj._id} user={userObj}
+    />
+  })
+  return (
+    <div className="container">
+      {userList}
+    </div>
+
+  )
+}
+
+export default UserList;
